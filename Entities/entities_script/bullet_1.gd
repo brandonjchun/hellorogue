e009db6e9866ace251acfe77d9b3639c2982781e
brandonjchun <zeonbravo@gmail.com>
2024-10-09 19:54:51 -0700

    fixed bug where gunshot audio cut short

--- Entities/entities_script/bullet_1.gd
+++ Entities/entities_script/bullet_1.gd
@@ -3,24 +3,35 @@ extends Area2D
 @onready var fx_scene = preload("res://Entities/Scenes/FX/fx_scene.tscn")
 @export var speed = 50
 var direction = Vector2.RIGHT
+var sfx_finished = false
+var was_body_entered = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$bullet_sound.finished.connect(on_sfx_finished)
 	$bullet_sound.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	translate(direction * speed * delta)
+	if sfx_finished and was_body_entered:
+		queue_free()
 
 func _on_body_entered(body):
-	Globals.camera.screen_shake(1,1,0.01)
-	instance_fx() 
-	queue_free()
+	if was_body_entered == false:
+		Globals.camera.screen_shake(1,1,0.01)
+		instance_fx() 
+		$CollisionShape2D.disabled = true
+		hide()
+		if sfx_finished:
+			queue_free()
+		else:
+			was_body_entered = true
 
-func _on_visible_screen_exited():
-	queue_free()
-	
 func instance_fx():
 	var fx = fx_scene.instantiate()
 	fx.global_position = global_position
 	get_tree().root.add_child(fx)
+
+func on_sfx_finished():
+	sfx_finished = true
